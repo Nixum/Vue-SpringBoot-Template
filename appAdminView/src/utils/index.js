05d90d4e1ae2cbf21def7b5b29f40c64d0dcd2d9
nixum <731604348@qq.com>
2019-02-16 00:42:51 +0800

    更新Vue项目和springboot项目这两个基础模板

---
+++ appAdminView/src/utils/index.js
@@ -0,0 +1,94 @@
+/**
+ * Unix时间戳转换成日期格式  unix2CurrentTime("1497232433000")
+ * @param unixTime Unix时间戳
+ * @return string yyyy-MM-dd HH:mm:ss
+ */
+export function unix2CurrentTime(unixTime) {
+  const date = new Date(parseInt(unixTime))
+  const y = date.getFullYear()
+  let m = date.getMonth() + 1
+  m = m < 10 ? ('0' + m) : m
+  let d = date.getDate()
+  d = d < 10 ? ('0' + d) : d
+  let h = date.getHours()
+  h = h < 10 ? ('0' + h) : h
+  let minute = date.getMinutes()
+  let second = date.getSeconds()
+  minute = minute < 10 ? ('0' + minute) : minute
+  second = second < 10 ? ('0' + second) : second
+  return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second
+}
+
+export function parseTime(time, cFormat) {
+  if (arguments.length === 0) {
+    return null
+  }
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if (('' + time).length === 10) time = parseInt(time) * 1000
+    date = new Date(time)
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    let value = formatObj[key]
+    if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+    if (result.length > 0 && value < 10) {
+      value = '0' + value
+    }
+    return value || 0
+  })
+  return time_str
+}
+
+/**
+ * 两个Unix时间戳差值
+ * @param unixTimeStart Unix时间戳1
+ * @param unixTimeEnd Unix时间戳2
+ * @return string xx 小时 | xx 天
+ */
+export function unixDifference(unixTimeStart, unixTimeEnd) {
+  const difference = (unixTimeEnd - unixTimeStart) / 1000
+  if (difference >= 86400) {
+    return difference / 86400 + '天'
+  } else if (difference >= 3600) {
+    return difference / 3600 + '小时'
+  } else if (difference >= 60) {
+    return difference / 60 + '分钟'
+  } else {
+    return difference + '秒'
+  }
+}
+
+/**
+ * 当前Unix时间戳差值
+ * @param unixTimeEnd Unix时间戳
+ * @return string | null xx天xx小时xx分钟xx秒
+ */
+export function nowDifference(unixTimeEnd) {
+  const unixTimeStart = new Date().getTime()
+  const difference = (unixTimeEnd - unixTimeStart) / 1000
+  if (difference > 0) {
+    let day = Math.floor(difference / (60 * 60 * 24))
+    let hour = Math.floor(difference / (60 * 60)) - (day * 24)
+    let minute = Math.floor(difference / 60) - (day * 24 * 60) - (hour * 60)
+    let second = Math.floor(difference) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60)
+    if (day <= 9) day = '0' + day
+    if (hour <= 9) hour = '0' + hour
+    if (minute <= 9) minute = '0' + minute
+    if (second <= 9) second = '0' + second
+    return day + '天' + hour + '小时' + minute + '分钟' + second + '秒'
+  } else {
+    return null
+  }
+}
---
+++ appServer/src/main/java/com/nixum/appServer/AppServerApplication.java
@@ -0,0 +1,14 @@
+package com.nixum.appServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServerApplication.class, args);
+	}
+
+}
+
